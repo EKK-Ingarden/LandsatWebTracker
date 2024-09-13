8742426ba7e7e379e27e26bd9b949c58e624de62
norbiros <norbiros@protonmail.com>
2024-09-13 21:18:03 +0200

    feat(frontend): Created pages directory

---
+++ frontend/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div h-screen flex items-center justify-center>
+    <div mx-auto max-w-xl text-center>
+      <div text-5xl>
+        Landsat Web Tracker
+      </div>
+      <div mt-4 text-lg text-gray-500>
+        Work in progress! View source code <a font-bold underline href="https://github.com/EKK-Ingarden/LandsatWebTracker">here</a><br>
+        Project made by EKK Ingarden for Nasa Space Apps 2024.
+      </div>
+    </div>
+  </div>
+</template>
