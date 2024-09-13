9ac835cc333c06e6323531d5a66326821f618cc9
norbiros <norbiros@protonmail.com>
2024-09-13 21:18:03 +0200

    feat(frontend): Created pages directory

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,17 +1,32 @@
 <template>
-  <div>
-    <h1 text-green>
-      LandsatWebTracker
-    </h1>
-
-    <Button @click="count++">
-      count is {{ count }}
-    </Button>
+  <div h-full>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Button } from "~/components/ui/button";
+useHead({
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" }
+  ],
+  link: [
+    { rel: "icon", href: "/favicon.ico" }
+  ],
+  htmlAttrs: {
+    lang: "en"
+  }
+});
 
-const count = ref(0);
+const title = "Landsat Web Tracker";
+const description = "Project made by EKK Ingarden for Nasa Space Apps 2024.";
+
+useSeoMeta({
+  title,
+  description,
+  ogTitle: title,
+  ogDescription: description
+});
 </script>
