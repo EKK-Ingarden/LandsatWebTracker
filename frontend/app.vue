76342459d1e6050db4ade647f5518fb6ecfeb561
norbiros <norbiros@protonmail.com>
2024-09-13 20:20:17 +0200

    feat(frontend): Shadcn component library

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,7 +1,17 @@
 <template>
-    <div>
-        <h1 text-green>
-            LandsatWebTracker
-        </h1>
-    </div>
+  <div>
+    <h1 text-green>
+      LandsatWebTracker
+    </h1>
+
+    <Button @click="count++">
+      count is {{ count }}
+    </Button>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { Button } from "~/components/ui/button";
+
+const count = ref(0);
+</script>
--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,35 +2,35 @@ import eslintConfig from "@antfu/eslint-config";
 import nuxtConfig from "./.nuxt/eslint.config.mjs";
 
 export default eslintConfig(
-    // General
-    {
-        typescript: true,
-        vue: true,
-        unocss: true,
-        stylistic: {
-            indent: 4,
-            quotes: "double"
-        },
-        rules: {
-            curly: "off",
-            "no-console": "off",
-            "no-new-func": "off",
-            "style/semi": ["error", "always"],
-            "style/indent": ["error", 4],
-            "style/quote-props": ["warn", "as-needed"],
-            "style/comma-dangle": ["warn", "never"],
-            "style/brace-style": ["warn", "1tbs"],
-            "style/arrow-parens": ["error", "always"],
-            "vue/block-order": ["error", {
-                order: ["template", "script", "style"]
-            }],
-            "vue/script-indent": ["error", 4, {
-                baseIndent: 1
-            }],
-            "antfu/top-level-function": "off",
-            "node/prefer-global/process": ["off"]
-        }
+  // General
+  {
+    typescript: true,
+    vue: true,
+    unocss: true,
+    stylistic: {
+      indent: 2,
+      quotes: "double"
     },
+    rules: {
+      curly: "off",
+      "no-console": "off",
+      "no-new-func": "off",
+      "style/semi": ["error", "always"],
+      "style/indent": ["error", 2],
+      "style/quote-props": ["warn", "as-needed"],
+      "style/comma-dangle": ["warn", "never"],
+      "style/brace-style": ["warn", "1tbs"],
+      "style/arrow-parens": ["error", "always"],
+      "vue/block-order": ["error", {
+        order: ["template", "script", "style"]
+      }],
+      "vue/script-indent": ["error", 2, {
+        baseIndent: 0
+      }],
+      "antfu/top-level-function": "off",
+      "node/prefer-global/process": ["off"]
+    }
+  },
 
-    nuxtConfig()
+  nuxtConfig()
 );
