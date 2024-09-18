0dafc668f883dcdd3024deacaecad61545659b5d
kubaryt <kubastarzyk6554@gmail.com>
2024-09-18 21:33:30 +0200

    enhancement: Added error handling and shadcn components for input and form - styles need fixing

--- frontend/components/WatchMyPixelForm.vue
+++ frontend/components/WatchMyPixelForm.vue
@@ -1,37 +1,51 @@
 <template>
   <div h-screen w="1/4" flex justify-center bg-black>
-    <div mx-auto mt-32 max-w-full flex flex-col text-left text-gray-300>
+    <div mx-auto mt-32 max-w-full flex flex-col items-center text-left text-gray-300>
       <div text-center text-4xl>
         Watch my pixel form
       </div>
-      <div mt-8 text-lg>
+      <div mt-8 flex flex-col items-center text-lg>
         <form flex flex-col items-center @submit.prevent="submitForm">
-          <input
-            id="name" v-model="formData.name" type="text" w="2/3" border-b-3 border-b-gray-700 rounded-sm bg-black
-            placeholder="Name" focus="outline-none" autocomplete="off"
-          >
-          <div w="2/3" mt-4 flex gap-10>
-            <input
-              id="lat" v-model="formData.lat" type="text" w="1/2" border-b-3 border-b-gray-700 rounded-sm bg-black
-              placeholder="Latitude" focus="outline-none" autocomplete="off"
-            >
-            <input
-              id="lng" v-model="formData.lng" type="text" w="1/2" border-b-3 border-b-gray-700 rounded-sm bg-black
-              placeholder="Longtitude" focus="outline-none" autocomplete="off"
-            >
-          </div>
-          <div mt-8 flex flex-col items-center>
-            <label for="date">Date to capture pixel</label>
-            <Calendar
-              v-model="formData.date"
-              calendar-label="Date to capture pixel"
-            />
-          </div>
-          <div mt-4>
-            <Button type="submit">
-              Submit
-            </Button>
-          </div>
+          <FormField name="username" :validate-on-blur="!isFieldDirty">
+            <FormItem>
+              <FormControl>
+                <Input v-model="formData.name" type="text" placeholder="Name" !w="full" />
+              </FormControl>
+            </FormItem>
+          </FormField>
+          <FormField name="username" :validate-on-blur="!isFieldDirty">
+            <FormItem w="full" flex flex-col items-center>
+              <FormControl>
+                <Popover>
+                  <PopoverTrigger as-child>
+                    <Button variant="outline" mt-4 w-full bg-black>
+                      {{ formData.date ? df.format(formData.date.toDate(getLocalTimeZone())) : "Date to capture pixel" }}
+                    </Button>
+                  </PopoverTrigger>
+                  <PopoverContent class="w-auto p-0">
+                    <Calendar v-model="formData.date" initial-focus />
+                  </PopoverContent>
+                </Popover>
+              </FormControl>
+            </FormItem>
+          </FormField>
+          <FormField name="latitude" :validate-on-blur="!isFieldDirty">
+            <FormItem>
+              <FormControl>
+                <Input v-model="formData.lat" type="text" placeholder="Latitude" />
+              </FormControl>
+            </FormItem>
+          </FormField>
+          <FormField name="longtitude" :validate-on-blur="!isFieldDirty">
+            <FormItem>
+              <FormControl>
+                <Input v-model="formData.lng" type="text" placeholder="Longtitude" />
+              </FormControl>
+            </FormItem>
+          </FormField>
+          <Button type="submit" mt-8>
+            Submit
+          </Button>
         </form>
       </div>
     </div>
@@ -39,6 +53,15 @@
 </template>
 
 <script setup>
+import { DateFormatter, getLocalTimeZone } from "@internationalized/date";
+import { toTypedSchema } from "@vee-validate/zod";
+import { useForm } from "vee-validate";
+import * as z from "zod";
+
+const df = new DateFormatter("en-US", {
+  dateStyle: "long"
+});
+
 const formData = ref({
   name: "",
   date: "",
@@ -46,7 +69,18 @@ const formData = ref({
   lng: ""
 });
 
-function submitForm() {
-  console.log(formData);
-};
+const formSchema = toTypedSchema(z.object({
+  username: z.string().min(2).max(50)
+}));
+
+const { isFieldDirty, handleSubmit } = useForm({
+  validationSchema: formSchema
+});
+
+const submitForm = handleSubmit((values) => {
+  toast({
+    title: "You submitted the following values:",
+    description: h("pre", { class: "mt-2 w-[340px] rounded-md bg-slate-950 p-4" }, h("code", { class: "text-white" }, JSON.stringify(values, null, 2)))
+  });
+});
 </script>
---
+++ frontend/components/ui/toast/ToastTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <ToastTitle v-bind="delegatedProps" :class="cn('text-sm font-semibold [&+div]:text-xs', props.class)">
+    <slot />
+  </ToastTitle>
+</template>
+
+<script setup lang="ts">
+import { type HTMLAttributes, computed } from "vue";
+import { ToastTitle, type ToastTitleProps } from "radix-vue";
+import { cn } from "@/lib/utils";
+
+const props = defineProps<ToastTitleProps & { class?: HTMLAttributes["class"] }>();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+</script>
