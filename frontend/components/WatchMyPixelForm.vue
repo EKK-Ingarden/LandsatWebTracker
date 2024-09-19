347d87e11cdf2ddc42c4f681883f0a1bb5e76120
norbiros <norbiros@protonmail.com>
2024-09-19 21:27:44 +0200

    idk

--- frontend/components/WatchMyPixelForm.vue
+++ frontend/components/WatchMyPixelForm.vue
@@ -6,8 +6,9 @@
       </div>
       <div mt-8 flex flex-col items-center text-lg>
         <form flex flex-col items-center w="15svw" @submit="submitForm">
-          <FormField v-slot="{ componentField }" name="username" :validate-on-blur="!isFieldDirty">
+          <FormField v-slot="{ componentField }" name="name" :validate-on-blur="!isFieldDirty">
             <FormItem w="full">
+              <FormMessage />
               <FormControl>
                 <Input v-bind="componentField" type="text" placeholder="Name" !w="full" />
               </FormControl>
@@ -15,31 +16,45 @@
           </FormField>
           <FormField v-slot="{ componentField }" name="date" :validate-on-blur="!isFieldDirty">
             <FormItem w="full" flex flex-col items-center>
+              <FormMessage />
               <FormControl>
                 <Popover>
                   <PopoverTrigger as-child>
                     <Button variant="outline" mt-4 w-full bg-black>
                       <CalendarIcon mr-2 h-4 w-4 />
-                      {{ formData.date ? df.format(formData.date.toDate(getLocalTimeZone())) : "Date to capture pixel" }}
+<!--                      {{ // dateValue.date ? df.format(toDate(dateValue)) : "Date to capture pixel" }}-->
                     </Button>
                   </PopoverTrigger>
                   <PopoverContent class="w-auto" p-0>
-                    <Calendar v-bind="componentField" initial-focus />
+                    <Calendar
+                      v-bind="componentField"
+                      v-model="dateValue"
+                      initial-focus
+                      @update:model-value="(v) => {
+                        if (v) {
+                          setFieldValue('date', v.toString())
+                        }
+                        else {
+                          setFieldValue('date', undefined)
+                        }
+                      }" />
                   </PopoverContent>
                 </Popover>
               </FormControl>
             </FormItem>
           </FormField>
           <div mt-4 flex gap-2>
-            <FormField v-slot="{ componentField }" name="latitude" :validate-on-blur="!isFieldDirty">
+            <FormField v-slot="{ componentField }" name="lat" :validate-on-blur="!isFieldDirty">
               <FormItem>
+                <FormMessage />
                 <FormControl>
                   <Input v-bind="componentField" type="text" placeholder="Latitude" !w="full" />
                 </FormControl>
               </FormItem>
             </FormField>
-            <FormField v-slot="{ componentField }" name="longtitude" :validate-on-blur="!isFieldDirty">
+            <FormField v-slot="{ componentField }" name="long" :validate-on-blur="!isFieldDirty">
               <FormItem>
+                <FormMessage />
                 <FormControl>
                   <Input v-bind="componentField" type="text" placeholder="Longtitude" !w="full" />
                 </FormControl>
@@ -56,22 +71,18 @@
 </template>
 
 <script setup>
-import { DateFormatter, getLocalTimeZone } from "@internationalized/date";
+import {DateFormatter, getLocalTimeZone, parseDate} from "@internationalized/date";
 import { toTypedSchema } from "@vee-validate/zod";
 import { useForm } from "vee-validate";
 import * as z from "zod";
 import { CalendarIcon } from "@radix-icons/vue";
+import {toDate} from "radix-vue/date";
 
 const df = new DateFormatter("en-US", {
   dateStyle: "long"
 });
 
-const formData = ref({
-  name: "",
-  date: "",
-  lat: "",
-  lng: ""
-});
+
 
 const formSchema = toTypedSchema(z.object({
   name: z.string().min(2).max(50),
@@ -80,9 +91,14 @@ const formSchema = toTypedSchema(z.object({
   lng: z.number().min(-180).max(180)
 }));
 
-const { isFieldDirty, handleSubmit } = useForm({
+const { isFieldDirty, handleSubmit, setFieldValue, values } = useForm({
   validationSchema: formSchema
 });
+
+const dateValue = computed({
+  get: () => values.date ? parseDate(values.date) : undefined,
+  set: (val) => val,
+})
 
 const submitForm = handleSubmit((values) => {
   console.log("dziengiel");
