60bab164991dc9d537246a6ea5f2190699c7431b
wikipop <66514885+wikipop@users.noreply.github.com>
2024-09-21 11:36:18 +0200

    feat: LandsatApi interface logic; landsat/search endpoint; project set for .env.production

--- backend/services/ReportGeneratorService.py
+++ backend/services/ReportGeneratorService.py
@@ -1,7 +1,4 @@
-
-
 class ReportGeneratorService:
     @staticmethod
     def generate_csv_report(self, data: None):
-        # TODO: data type is None cos I am going to sleep refactor this later
         raise NotImplementedError
