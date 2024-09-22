ed6195d03b43f801e874bd9de618548027376e4b
norbiros <norbiros@protonmail.com>
2024-09-22 14:06:06 +0200

    feat(backend): Switch to supabase authorization & users

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,11 +1,18 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import JSON, UUID, Boolean, Column, String, text
 
-from ..database import Base
+from backend.database import Base
 
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = 'users'
+    __table_args__ = {'schema': 'auth'}
 
-    id = Column(Integer, primary_key=True)
-    email = Column(String, unique=True, index=True)
-    username = Column(String)
+    id = Column(UUID,
+                server_default=text('auth.gen_random_uuid()'),
+                primary_key=True)
+    email = Column(String, unique=True)
+    encrypted_password = Column(String)
+    role = Column(String, nullable=False)
+    raw_app_meta_data = Column(JSON)
+    raw_user_meta_data = Column(JSON)
+    is_anonymous = Column(Boolean, nullable=False, default=True)
--- backend/schemas/structures/user.py
+++ backend/schemas/structures/user.py
@@ -1,10 +1,23 @@
-from pydantic import BaseModel
+from typing import Optional
+from uuid import UUID
+
+from pydantic import BaseModel, EmailStr
 
 
-class User(BaseModel):
-    id: int
-    email: str
-    username: str
+class UserBase(BaseModel):
+    id: UUID
+    email: Optional[EmailStr]
+
+    class Config:
+        from_attributes = True
+
+
+class User(UserBase):
+    encrypted_password: str
+    role: str
+    raw_app_meta_data: Optional[dict]
+    raw_user_meta_data: Optional[dict]
+    is_anonymous: bool
 
     class Config:
         from_attributes = True
