7ec09bfd47a2f6aa2a77651909601e2d23184e04
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2024-09-24 08:36:20 +0200

    feat: Created error page (#37)

---
+++ frontend/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <NuxtLayout>
+    <div class="bg-[url(assets/img/background.png)]" h-screen w-screen bg-cover bg-no-repeat>
+      <div h-full w-full flex flex-col items-center justify-center bg-black bg-opacity-85>
+        <h1 text-7xl>
+          Error {{ error?.statusCode }}
+        </h1>
+        <p mt-4 text-2xl text-gray-400>
+          {{ error?.statusMessage }}
+        </p>
+        <Button variant="grayDefault" size="lg" text-size="base" mt-6>
+          <NuxtLink to="/">
+            Go back home
+          </NuxtLink>
+        </Button>
+      </div>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps({
+  error: Object as () => NuxtError
+});
+</script>
