223bbdc5264ccf64672bc0d8aab77a295dd3055b
norbiros <norbiros@protonmail.com>
2024-09-28 11:52:42 +0200

    feat: Show Jelenia Góra tile

--- frontend/pages/map.client.vue
+++ frontend/pages/map.client.vue
@@ -5,13 +5,13 @@
 <script setup lang="ts">
 import { LatLng } from "leaflet";
 
-const polygon: LatLng[] = [new LatLng(0, 0), new LatLng(1, 1), new LatLng(5000, 500)];
+const polygon: LatLng[] = [new LatLng(51.342800656448965, 14.370216927521078), new LatLng(49.64140114815556, 13.69019407190872), new LatLng(49.20099186105633, 16.184743219759543), new LatLng(50.89540578366285, 16.955436093090825), new LatLng(51.342800656448965, 14.370216927521078)];
 const image = {
-  url: "https://www.printablee.com/postpic/2011/06/blank-100-square-grid-paper_405041.jpg",
-  bounds: [[-20, -15], [10, 15]] as L.LatLngBoundsLiteral
+  url: "https://planetarycomputer.microsoft.com/api/data/v1/item/preview.png?collection=landsat-c2-l2&item=LC09_L2SP_191025_20240919_02_T1&assets=red&assets=green&assets=blue&color_formula=gamma+RGB+2.7%2C+saturation+1.5%2C+sigmoidal+RGB+15+0.55&format=png",
+  bounds: polygon.map(({ lat, lng }) => [lat, lng]) as L.LatLngBoundsLiteral
 };
 
-function mapClick(e) {
-  console.log("Clicked at", e);
+function mapClick(location: LatLng) {
+  console.log("Clicked at", location);
 }
 </script>
