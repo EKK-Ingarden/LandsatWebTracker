7772cf82f5ad038675aa6164f8f8fe17edd9e5d3
wikipop <66514885+wikipop@users.noreply.github.com>
2024-09-28 12:32:22 +0200

    enhancement: settings for usgs key

--- backend/settings.py
+++ backend/settings.py
@@ -4,6 +4,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     database_url: str = "postgresql://user:password@localhost:5432/landsat_web_tracker_db"
     landsat_provider_url: str = "https://planetarycomputer.microsoft.com/api/stac/v1"
+    usgs_m2m_api_key: str = "<api key>"
+    nuxt_open_fetch_api_base_url: str = "http://localhost:8000"
     supabase_url: str = "https://<id>.supabase.co"
     supabase_key: str = "<secret key>"
 
