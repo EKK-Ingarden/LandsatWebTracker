2a3a3c7a6849ec9aa05fb05d2f449739f5a80b9a
norbiros <norbiros@protonmail.com>
2024-09-29 19:20:50 +0200

    feat: Endpoints for `get_acquisitions` & `get_scene`

--- backend/utils/utils.py
+++ backend/utils/utils.py
@@ -3,7 +3,6 @@ import json
 from fastapi import FastAPI
 
 from backend.routers.auth_router import auth_router
-from backend.routers.coordinates_router import coordinates_router
 from backend.routers.index_router import index_router
 from backend.routers.landsat_api_router import landsat_api_router
 
@@ -11,7 +10,6 @@ from backend.routers.landsat_api_router import landsat_api_router
 def create_app():
     new_app = FastAPI(title="Landsat Web Tracker API")
     new_app.include_router(index_router, prefix="")
-    new_app.include_router(coordinates_router, prefix="/coordinates")
     new_app.include_router(landsat_api_router, prefix="/landsat")
     new_app.include_router(auth_router, prefix="/auth")
 
