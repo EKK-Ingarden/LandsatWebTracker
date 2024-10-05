b21657e5ae5f762de6d0843347040be2cd26b4cb
norbiros <norbiros@protonmail.com>
2024-10-05 19:11:48 +0200

    feat: wip

--- frontend/components/Arch.vue
+++ frontend/components/Arch.vue
@@ -1,6 +1,5 @@
 <template>
-  <svg class="arch" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 20" preserveAspectRatio="none">
-    <path d="M 0 20 Q 50 15 100 20" :stroke="`url(#grad${archNumber})`" :stroke-width="strokeWidth" fill="transparent" />
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 15 100 7" preserveAspectRatio="none">
     <defs>
       <linearGradient :id="`grad${archNumber}`" x1="0%" y1="0%" x2="100%" y2="0%">
         <stop offset="0%" style="stop-color:#6983EA; stop-opacity:1" />
@@ -8,6 +7,7 @@
         <stop offset="100%" style="stop-color:#9EF01A; stop-opacity:1" />
       </linearGradient>
     </defs>
+    <path d="M 0 20 Q 50 15 100 20" :stroke="`url(#grad${archNumber})`" :stroke-width="strokeWidth" fill="transparent" />
   </svg>
 </template>
 
