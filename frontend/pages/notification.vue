7092e42a92d734e5875592a106bf93b40aa59a35
Wojciech Piech <ilikeahottodogu@gmail.com>
2024-10-06 10:47:01 +0200

    Notification Page

--- frontend/pages/notification.vue
+++ frontend/pages/notification.vue
@@ -1,63 +1,63 @@
-<script setup lang="ts">
+<template>
+  <div mx-35 mt-20>
+    <p text-xl>
+      Incoming Notifications
+    </p>
 
+    <div mt-8 border rounded-md border-color="#737373">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Date</TableHead>
+            <TableHead>Row</TableHead>
+            <TableHead>Path</TableHead>
+            <TableHead>Latitude</TableHead>
+            <TableHead>Longitude</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow v-for="(notification, index) in notifications" :key="notification.row">
+            <TableCell>{{ notification.date }}</TableCell>
+            <TableCell>{{ notification.row }}</TableCell>
+            <TableCell>{{ notification.path }}</TableCell>
+            <TableCell>{{ notification.latitude }}</TableCell>
+            <TableCell>{{ notification.longitude }}</TableCell>
+            <TableCell>
+              <button class="text-red-500 hover:text-red-700" @click="deleteNotification(index)">
+                DELETE
+              </button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
 import {
   Table,
   TableBody,
   TableCell,
   TableHead,
   TableHeader,
-  TableRow,
-} from '@/components/ui/table'
+  TableRow
+} from "@/components/ui/table";
 
 const notifications = ref([
   {
-    date: "11.9.2024", row: 21, path: 37, latitude: 69, longitude: 88,
+    date: "11.9.2024",
+    row: 21,
+    path: 37,
+    latitude: 69,
+    longitude: 88
   }
-])
+]);
 
 const deleteNotification = (rowIndex: number) => {
   notifications.value.splice(rowIndex, 1);
 };
-
-
 </script>
-
-<template>
-<div mt-20 mx-35>
-  <p text-xl>
-    Incoming Notifications
-  </p>
-
-  <div mt-8 border rounded-md border-color="#737373">
-    <Table>
-      <TableHeader>
-        <TableRow>
-          <TableHead>Date</TableHead>
-          <TableHead>Row</TableHead>
-          <TableHead>Path</TableHead>
-          <TableHead>Latitude</TableHead>
-          <TableHead>Longitude</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow v-for="(notification, index) in notifications" :key="notification.row">          <TableCell>{{notification.date}}</TableCell>
-          <TableCell>{{ notification.row }}</TableCell>
-          <TableCell>{{ notification.path }}</TableCell>
-          <TableCell>{{ notification.latitude }}</TableCell>
-          <TableCell>{{ notification.longitude }}</TableCell>
-          <TableCell>
-          <button @click="deleteNotification(index)" class="text-red-500 hover:text-red-700">
-            DELETE
-            </button>
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
-  </div>
-</div>
-</template>
-
-
 
 <style scoped lang="scss">
 
