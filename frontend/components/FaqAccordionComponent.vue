7e0202e84cb88cb4c4d4138c40a9320447e9f7c9
Wojciech Piech <129212595+wojpo@users.noreply.github.com>
2024-10-06 07:34:47 +0200

    [FEAT]: FAQ page (#95)
    * feat: Backend temperature chart
    
    * FAQ site (work in progress)
    
    * FaqAccordionComponent and fixed styling
    
    * [fix]: :3 FIX
    
    ---------
    
    Co-authored-by: Wiktor Popiołek <66514885+wikipop@users.noreply.github.com>

---
+++ frontend/components/FaqAccordionComponent.vue
@@ -0,0 +1,27 @@
+<template>
+  <div bg="#232323" mt-6 border-rd-sm>
+    <Accordion type="single" collapsible mx-4>
+      <AccordionItem value=":3">
+        <AccordionTrigger>
+          <p>{{ item.question }}</p>
+        </AccordionTrigger>
+        <AccordionContent>
+          <p color="#a3a3a3">
+            {{ item.answer }}
+          </p>
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+
+defineProps<{
+  item: {
+    question: string
+    answer: string
+  }
+}>();
+</script>
---
+++ frontend/pages/faq.vue
@@ -0,0 +1,41 @@
+<template>
+  <div mx-auto w-full overflow-x-hidden bg-black bg-opacity-85 full-height-without-header>
+    <div mx-19 mt-6>
+      <p text-6xl font-950>
+        FAQ
+      </p>
+      <p text-5xl font-850 color="#d4d4d4">
+        How can we help?
+      </p>
+    </div>
+    <div mt-10 flex-col bg-cover bg-center>
+      <div v-for="(item, index) in faqList" :key="`key-${index}`" mx-19 mt-6>
+        <AccordionItemComponent :item="item" />
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script setup lang="ts">
+import AccordionItemComponent from "@/components/FaqAccordionComponent.vue";
+
+const faqList = [
+  {
+    question: "How often do Landsat satellites pass over the same location?",
+    answer: "Landsat satellites typically revisit the same location every 16 days. However, by using both Landsat 8 and Landsat 9, users can effectively get coverage every 8 days for most regions."
+  },
+  {
+    question: "What does Landsat data show?",
+    answer: "Landsat data provides imagery that captures visible and infrared light reflected off the Earth's surface. This data can be used to study vegetation, urban growth, water bodies, forests, agricultural lands, and more."
+  },
+  {
+    question: "How do I define my target location?",
+    answer: "To define your target location, you can use coordinates or select from a map interface provided in the application."
+  },
+  {
+    question: "Do I need to install any software to use the application?",
+    answer: "No, you can access Landsat data directly through web applications without needing to install any software."
+  }
+];
+</script>
