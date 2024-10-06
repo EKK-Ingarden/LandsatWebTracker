2db0b347716023683f7ddf69abe020f130e523d2
Wojciech Piech <129212595+wojpo@users.noreply.github.com>
2024-10-06 10:51:14 +0200

    [Feat]: Notifications page (#103)
    * enhancement: Improved landing site (#97)
    
    * Notification Page
    
    * Notification Page
    
    ---------
    
    Co-authored-by: Kubaryt <88268063+Kubaryt@users.noreply.github.com>

---
+++ frontend/pages/notification.vue
@@ -0,0 +1,64 @@
+<template>
+  <div mx-35 mt-20>
+    <p text-xl>
+      Incoming Notifications
+    </p>
+
+    <div mt-8 border rounded-md border-color="#737373">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Date</TableHead>
+            <TableHead>Row</TableHead>
+            <TableHead>Path</TableHead>
+            <TableHead>Latitude</TableHead>
+            <TableHead>Longitude</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow v-for="(notification, index) in notifications" :key="notification.row">
+            <TableCell>{{ notification.date }}</TableCell>
+            <TableCell>{{ notification.row }}</TableCell>
+            <TableCell>{{ notification.path }}</TableCell>
+            <TableCell>{{ notification.latitude }}</TableCell>
+            <TableCell>{{ notification.longitude }}</TableCell>
+            <TableCell>
+              <button class="text-red-500 hover:text-red-700" @click="deleteNotification(index)">
+                DELETE
+              </button>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow
+} from "@/components/ui/table";
+
+const notifications = ref([
+  {
+    date: "11.9.2024",
+    row: 21,
+    path: 37,
+    latitude: 69,
+    longitude: 88
+  }
+]);
+
+const deleteNotification = (rowIndex: number) => {
+  notifications.value.splice(rowIndex, 1);
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
