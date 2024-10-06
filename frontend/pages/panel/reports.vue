2cc0ae9a22abc47e43c954e8279cb294bc2fc980
Kubaryt <88268063+Kubaryt@users.noreply.github.com>
2024-10-06 17:08:06 +0200

    minor changes and fixes (#117)

--- frontend/pages/panel/reports.vue
+++ frontend/pages/panel/reports.vue
@@ -1,6 +1,6 @@
 <template>
   <div mx-35 mt-20>
-    <h1 text-10>
+    <h1 text-2xl>
       Reports
     </h1>
     <span>Your generated reports</span>
@@ -12,7 +12,7 @@
             <TableHead>Satelite</TableHead>
             <TableHead>Date</TableHead>
             <TableHead>Status</TableHead>
-            <TableHead>Link</TableHead>
+            <TableHead><!-- extends table header to full width --></TableHead>
             <TableHead><!-- extends table header to full width --></TableHead>
           </TableRow>
         </TableHeader>
@@ -21,7 +21,7 @@
             <TableCell>{{ report.satelite }}</TableCell>
             <TableCell>{{ report.date }}</TableCell>
             <TableCell>{{ report.status }}</TableCell>
-            <TableCell><NuxtLink to="{{ report.link }}">Link</NuxtLink></TableCell>
+            <TableCell><NuxtLink :to="`/panel/report/${report.id}`"><img class="i-material-symbols-light:open-in-new" text-2xl></NuxtLink></TableCell>
             <TableCell>
               <button class="text-red-500 hover:text-red-700" @click="deleteReport(index)">
                 DELETE
@@ -40,7 +40,7 @@ const reports = ref([
     satelite: "Landsat 9",
     date: "11.9.2024",
     status: "Finished",
-    link: "/panel/report/id"
+    id: "1"
   }
 ]);
 
