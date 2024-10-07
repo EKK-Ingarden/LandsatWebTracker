3f4b4dae4a4e2730475e1f97cffb8a231281173e
wikipop <66514885+wikipop@users.noreply.github.com>
2024-10-07 18:58:36 +0200

    fix: reflectance returned properly

--- backend/tasks/write_report.py
+++ backend/tasks/write_report.py
@@ -1,5 +1,3 @@
-import sys
-
 from sqlalchemy.orm import Session
 
 from backend.models.report import Report
@@ -7,15 +5,11 @@ from backend.schemas.landsat.landsat_api_by_id_advanced import LandsatAdvancedAP
 
 
 def write_report_to_db(scene_id: str, db: Session):
-    print("working")
     landsat_item = LandsatAdvancedAPIById(scene_id=scene_id).first_result
-    print("afterwaited")
-    print(sys.getsizeof(landsat_item.model_dump_json()))
-    db.query(Report).filter(Report.scene_id == scene_id).update(
+    db.query(Report).filter(scene_id == Report.scene_id).update(
         {
             "is_processed": True,
             "raw_data": landsat_item.model_dump_json(),
         }
     )
-    print("dumped")
     db.commit()
