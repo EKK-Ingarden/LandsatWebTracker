f7a254ad03b934fa98e6e71d3d8ff946af748e31
norbiros <norbiros@protonmail.com>
2024-10-08 17:30:06 +0200

    feat: Basic reports

--- backend/schemas/structures/coordinates.py
+++ backend/schemas/structures/coordinates.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel, Field
+from pydantic import AliasChoices, BaseModel, Field
 
 
 class Coordinates(BaseModel):
-    latitude: float = Field(alias="lat", ge=-90, le=90)
-    longitude: float = Field(alias="lon", ge=-180, le=180)
+    latitude: float = Field(..., validation_alias=AliasChoices("lat", "latitude"), ge=-90, le=90)
+    longitude: float = Field(..., validation_alias=AliasChoices("lon", "longitude"), ge=-180, le=180)
 
     def to_list(self):
         return [self.latitude, self.longitude]
