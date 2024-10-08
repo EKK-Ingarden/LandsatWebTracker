0cc4948354bc4e5097a74227361ef893621fdc29
norbiros <norbiros@protonmail.com>
2024-10-08 17:30:06 +0200

    feat: Basic reports

--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -4,6 +4,7 @@ from .structures.cloud_coverage_ratio import CloudCoverageRatio
 from .structures.coordinates import Coordinates
 from .structures.pixel_watch import PixelWatch
 from .structures.polygon import Polygon
+from .structures.report import Report
 from .structures.satellite import Satellite
 from .structures.tile_attributes import TileAttributes
 from .structures.user import User, UserBase
---
+++ backend/schemas/structures/report.py
@@ -0,0 +1,14 @@
+import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class Report(BaseModel):
+    scene_id: str
+    is_processed: bool
+    created_at: datetime.datetime
+    raw_data: Optional[str]
+
+    class Config:
+        from_attributes = True
